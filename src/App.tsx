27e71bc0954c1a074fa7e669e066fe6501d1376b
geowarin <geowarin@gmail.com>
2018-01-16 19:28:40 +0100

    Hello parcel react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+export class App extends React.Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello</h1>
+      </React.Fragment>
+    )
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+
+import {render} from "react-dom";
+import {App} from "./App";
+import * as React from "react";
+
+render(
+  <App />,
+  document.getElementById("root")
+);
